9f0a23b6377da8765772fcd97d6eaac135e5a43f
Pguin12 <74618829+Pguin12@users.noreply.github.com>
2020-11-17 17:16:36 -0500

    Add files via upload

---
+++ New folder/pronouns.py
@@ -0,0 +1,4 @@
+def pronouns(one, two, three, four):
+    print(one, "is a really cool person", two,"pronouns are awesome. I like that name of",three,"too! I am glad I know", two,"and hope",one,"takes care of", four, "today")
+
+
